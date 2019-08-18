cf27ca64e811e2a3df84699a5d84c3d5e567afde
cesare <cesare.marchesi@yahoo.it>
2019-08-18 18:13:47 +0200

    Updates

---
+++ precache-manifest.4efef10bea25b1ae6892c5e9802722e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820622625f48b2b406a55b7ea76335b3",
+    "url": "/meetup/index.html"
+  },
+  {
+    "revision": "aada6dd3c53276871c3b",
+    "url": "/meetup/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "c638261f5ed0aa248d0a",
+    "url": "/meetup/static/js/2.14e23e62.chunk.js"
+  },
+  {
+    "revision": "aada6dd3c53276871c3b",
+    "url": "/meetup/static/js/main.cadad242.chunk.js"
+  },
+  {
+    "revision": "0e233e70227c3f52b786",
+    "url": "/meetup/static/js/runtime~main.3bd9edc5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/meetup/static/media/logo.5d5d9eef.svg"
+  }
+]);
